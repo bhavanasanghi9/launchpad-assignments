cf4837e1330d33be28a5854f1be20e986269a43d
bhavanasanghi9 <61513642+bhavanasanghi9@users.noreply.github.com>
2020-02-26 23:48:02 +0530

    Add files via upload

---
+++ problem6.py
@@ -0,0 +1,17 @@
+record={}
+name1=input("enter your name")
+usn1=input("enter your usn")
+name2=input("enter your name")
+usn2=input("enter you usn")
+name3=input("enter your name")
+usn3=input("enter your usn")
+key1=name1
+key2=name2
+key3=name3
+value1=usn1
+value2=usn2
+value3=usn3
+record[key1]=value1
+record[key2]=value2
+record[key3]=value3
+print(record)
