df986f2c670564bd9ea25b76cb4c5bf97c3f6a44
bhavanasanghi9 <61513642+bhavanasanghi9@users.noreply.github.com>
2020-05-11 12:17:43 +0530

    Add files via upload

---
+++ lowest.py
@@ -0,0 +1,21 @@
+num_list=[]
+"""let the number of elements of the list be n"""
+
+n=int(input("enter the number of elements in the list"))
+"""ele represents each element in the list that we are going to input"""
+for i in range (0,n):
+	ele=int(input("enter the elements of the list"))
+	num_list.append(ele)
+"""printing out the given list in descending order"""
+num_list.sort(reverse=True)
+
+"""this input function will accept the value of n for nth lowest number"""
+nth_lowest=int(input("enter the value of n to find the nth lowest number"))
+if nth_lowest>len(num_list):
+	print("there is an error")
+else:
+
+	for j in num_list:
+		print(f"the {nth_lowest}th lowest number is {num_list[nth_lowest-1]}")
+		break
+"""j being each element iterating in num_list"""
